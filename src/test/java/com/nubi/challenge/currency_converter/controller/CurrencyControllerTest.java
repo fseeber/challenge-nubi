95906b2884e8c4d9bbb722db0b3dfbba1eaec04b
Federico Seeber <federico.seeber@enerminds.com>
2025-02-18 07:29:29 -0300

    se cambia app por consola para manejar mejor los errores

--- src/test/java/com/nubi/challenge/currency_converter/controller/CurrencyControllerTest.java
+++ src/test/java/com/nubi/challenge/currency_converter/controller/CurrencyControllerTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @WebMvcTest(CurrencyController.class)
-@AutoConfigureMockMvc(addFilters = false) // Desactiva los filtros de seguridad
+@AutoConfigureMockMvc(addFilters = false) 
 class CurrencyControllerTest {
 
        @Autowired
@@ -29,11 +29,9 @@ class CurrencyControllerTest {
 
        @Test
        void testConvertCurrencySuccess() throws Exception {
-              // Mock del servicio para un caso exitoso
               Mockito.when(currentService.convertCurrency("USD", "EUR", 100.0))
                      .thenReturn("200.00 EUR");
 
-              // Realiza la solicitud y verifica la respuesta
               mockMvc.perform(post("/convert")
                      .contentType(MediaType.APPLICATION_JSON)
                      .content("{\"baseCurrency\":\"USD\",\"targetCurrency\":\"EUR\",\"amount\":100}"))
@@ -44,11 +42,9 @@ class CurrencyControllerTest {
 
        @Test
        void testConvertCurrencyInvalidAmount() throws Exception {
-              // Mock del servicio para lanzar una excepción de cantidad inválida
               Mockito.when(currentService.convertCurrency(Mockito.anyString(), Mockito.anyString(), Mockito.eq(-10.0)))
                      .thenThrow(new InvalidAmountException("La cantidad debe ser mayor a cero."));
 
-              // Realiza la solicitud y verifica el manejo de la excepción
               mockMvc.perform(post("/convert")
                      .contentType(MediaType.APPLICATION_JSON)
                      .content("{\"baseCurrency\":\"USD\",\"targetCurrency\":\"EUR\",\"amount\":-10}"))
@@ -59,11 +55,9 @@ class CurrencyControllerTest {
 
        @Test
        void testConvertCurrencyInvalidCurrency() throws Exception {
-              // Mock del servicio para lanzar una excepción de moneda inválida
               Mockito.when(currentService.convertCurrency("XYZ", "EUR", 100.0))
                      .thenThrow(new InvalidCurrencyException("La moneda XYZ no es válida."));
 
-              // Realiza la solicitud y verifica el manejo de la excepción
               mockMvc.perform(post("/convert")
                      .contentType(MediaType.APPLICATION_JSON)
                      .content("{\"baseCurrency\":\"XYZ\",\"targetCurrency\":\"EUR\",\"amount\":100}"))
@@ -81,17 +75,15 @@ class CurrencyControllerTest {
               .contentType(MediaType.APPLICATION_JSON)
               .content("{\"baseCurrency\":\"USD\",\"targetCurrency\":\"EUR\",\"amount\":100}"))
               .andDo(print())
-              .andExpect(status().isGatewayTimeout())  // Verificamos el 504
+              .andExpect(status().isGatewayTimeout())  
               .andExpect(content().string("Error de tiempo de espera: La API externa no respondió a tiempo."));
        }
 
        @Test
        void testConvertCurrencyInternalError() throws Exception {
-              // Mock del servicio para lanzar una excepción genérica
               Mockito.when(currentService.convertCurrency("USD", "EUR", 100.0))
                      .thenThrow(new RuntimeException("Error inesperado."));
 
-              // Realiza la solicitud y verifica el manejo del error interno
               mockMvc.perform(post("/convert")
                      .contentType(MediaType.APPLICATION_JSON)
                      .content("{\"baseCurrency\":\"USD\",\"targetCurrency\":\"EUR\",\"amount\":100}"))
